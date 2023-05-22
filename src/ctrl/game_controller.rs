8e7c1acc18898fb96cf0153966f1507b00397e44
XUChenghao <xuchenghao@xch.local>
2023-05-22 21:32:07 +0800

    earnings

--- src/ctrl/game_controller.rs
+++ src/ctrl/game_controller.rs
@@ -204,5 +204,10 @@ fn timestamp() -> i64 {
 }
 
 pub async fn query_last_day_earning_reports(pool: &Pool<MySql>, game_service: &GameService) {
-    game_service.query_last_day_earning_reports(pool).await;
+    let time = timestamp() % 86400;
+    println!("query_last_day_earning_reports {}", time);
+    if time >= 3600 && time < 4500 {
+        game_service.query_last_day_earning_reports(pool).await;
+    }
+    
 }
